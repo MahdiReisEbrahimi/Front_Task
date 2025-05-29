fa85ba186318a707f42e4b1181551d92a7856024
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-05-29 14:09:35 +0330

    some change on next.config to accept new img src

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["reqres.in"],
+    domains: ["reqres.in", "tinyjpg.com"],
   },
 };
 
