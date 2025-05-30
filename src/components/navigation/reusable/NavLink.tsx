815b9a387351a54adbe84ed90036006fa91e2f93
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-05-31 00:31:50 +0330

    signup and login current opening bug fixed in laptop screen -> in nav

--- src/components/navigation/reusable/NavLink.tsx
+++ src/components/navigation/reusable/NavLink.tsx
@@ -1,23 +1,27 @@
 import { BsFillPersonVcardFill } from "react-icons/bs";
 import { PiUsersFill } from "react-icons/pi";
 import Link from "next/link";
-import { useSelector, UseSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { RootState } from "@/store";
 
 interface NavLinksType {
   label: string;
   pathname: string;
 }
+
+// reusable link component for navigation Links.
 export default function NavLink({ label, pathname }: NavLinksType) {
   const auth = useSelector((state: RootState) => state.auth);
 
   return (
+    // if pathname was equal to every link -> that link will have a selected className
     <Link
       href={label == "Users" ? "/" : `${auth.user.id}`}
       className={`w-full text-white flex items-center font-bold text-xl py-2 px-2 rounded hover:bg-gray-800 cursor-pointer transition ${
         label === "Users" && pathname === "/" && "border-l-4"
-      } ${label === "Profile" && pathname !== "/" && 'border-l-4'}`}
+      } ${label === "Profile" && pathname !== "/" && "border-l-4"}`}
     >
+      {/* to select correct icon */}
       {label == "Profile" && <BsFillPersonVcardFill className="ml-6" />}
       {label == "Users" && <PiUsersFill className="ml-6" />}
 
